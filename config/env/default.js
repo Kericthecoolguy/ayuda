aa2eeb2c38111f1d98ebc724b7d3359ae9146fac
Keric Edwards <kericthecoolguy@gmail.com>
2016-02-16 22:11:17 -0500

    testing server email

--- config/env/default.js
+++ config/env/default.js
@@ -13,5 +13,6 @@ module.exports = {
   sessionCollection: 'sessions',
   logo: 'modules/core/img/brand/logo.png',
   favicon: 'modules/core/img/brand/favicon.ico',
-  smtp: 'smtps://drexelayuda%40gmail.com:Ayuda2016@smtp.gmail.com'
+  //smtp: 'smtps://drexelayuda%40gmail.com:Ayuda2016@smtp.gmail.com'
+  smtp: 'smtps://drexelayuda:Ayuda2016@smtp.gmail.com'
 };
