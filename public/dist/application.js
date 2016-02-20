41205f06650d756baca036afb07aa4080ac4419d
Keric Edwards <kericthecoolguy@gmail.com>
2016-02-20 15:54:43 -0500

    prod test

--- public/dist/application.js
+++ public/dist/application.js
@@ -104,20 +104,20 @@ angular.module('articles').run(['Menus',
   function (Menus) {
     // Add the articles dropdown item
     Menus.addMenuItem('topbar', {
-      title: 'Articles',
+      title: 'Posts',
       state: 'articles',
       type: 'dropdown'
     });
 
     // Add the dropdown list item
     Menus.addSubMenuItem('topbar', 'articles', {
-      title: 'List Articles',
+      title: 'List Posts',
       state: 'articles.list'
     });
 
     // Add the dropdown create item
     Menus.addSubMenuItem('topbar', 'articles', {
-      title: 'Create Articles',
+      title: 'Create Post',
       state: 'articles.create'
     });
   }
